c1d3fa7fae22da7b1f30358343d70dea0e5e6cb1
Kimsc9976 <tncks097@naver.com>
2022-12-25 02:09:07 +0900

    Todo make logout N simple admin page

--- certificate.js
+++ certificate.js
@@ -5,36 +5,61 @@ const topic = require('./lib/topic');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const logic = require('./lib/logic');
+const j = require('./lib/jwt');
+
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static('./src'));
 
+const dataBuff = fs.readFileSync(process.cwd() + '/src/inform/user_data.json');
+const dataJSON = dataBuff.toString();
+let data = JSON.parse(dataJSON);
 
-app.get('/', (req, res) =>
+app.get('/', async (req, res) =>
 {
   // 토큰이 없으면
-  res.sendFile(process.cwd() + '/src/data/sign_in.html');
+  let verifyed_token = await j.verify(data.accessToken);
 
+  if(verifyed_token === -2 || verifyed_token === -3)
+  {
+    console.log("sign in page");
+    res.sendFile(process.cwd() + '/src/data/sign_in.html');
+  }
   // 토큰이 있으면
+  else if(verifyed_token)
+  {
+    console.log("log in completed");
+    res.sendFile(process.cwd() + '/src/data/blog.html');
+  }
+})
+
+app.post('/sign_in', (req, res) =>
+{
+  topic.Signin_process(req, res);
   
 })
 
 app.get('/sign_up', (req, res) =>
 {
+  console.log("sign up page");
   res.sendFile(process.cwd() + '/src/data/sign_up.html');
 })
 
-app.get('/sign_in', (req, res) =>
+app.get('/IDdupli', async (req, res) =>
 {
-  res.sendFile(process.cwd() + '/src/data/sign_up.html');
-})
+  let url = req.url;
+  let query = URL.parse(url,true).query;
+  let result  = await logic.ID_confirm(query.ID);
 
-// 그 뭐시야 중복체크 확인 해야함
-app.get('/IDdupli', (req, res) =>
-{
-  let post = req.body;
-
-  console.log(post);
-  res.send("ID_Duplication");
+  let config = result[0], msg = result[1];
+  if(config)
+  {
+    res.send(logic.alert_msg(msg));
+  }
+  if(!config)
+  {
+    res.send(logic.alert_msg(msg));
+  }
 })
 
 app.post('/sign_up', (req, res) =>
@@ -42,9 +67,5 @@ app.post('/sign_up', (req, res) =>
   topic.Signup_process(req, res);
 })
 
-app.post('/sign_in', (req, res) =>
-{
-  topic.Signin_process(req, res);
-})
 
 app.listen(3000, () => console.log("example app "));
